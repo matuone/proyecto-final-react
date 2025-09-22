0f1d3855ee36c92c408401801c6abac8df4b742a
matiascastells <matiascastells@gmail.com>
2025-09-22 19:31:13 -0300

    boton de mostrar y ocultar contraseña

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -6,12 +6,17 @@ const Login = () => {
   const [password, setPassword] = useState()
   const [message, setMessage] = useState()
   const [error, setError] = useState()
+
+  const [showPassword, setShowPassword] = useState()
+
   const navigate = useNavigate()
 
   // hook -> anzuelo -> una funcionalidad que provee react
   // use -> "usando ..."
   // useState -> usando un estado
   // un estado es una variable que cuando cambiar se rerenderiza el componente
+
+  // let showPassword = false
 
   const PASS = "pepe123"
 
@@ -34,13 +39,20 @@ const Login = () => {
     validatePassword()
   }
 
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword)
+  }
+
   return (
     <main className="login-main">
       <img width={100} src={logo} alt="logo de whatsapp" />
       <h1>Clon de Whatsapp</h1>
       <form onSubmit={handleSubmit}>
         <label>Contraseña de acceso</label>
-        <input placeholder="Ingrese la contraseña" type="password" onChange={(event) => setPassword(event.target.value)} />
+        <input placeholder="Ingrese la contraseña"
+          type={showPassword ? "text" : "password"}
+          onChange={(event) => setPassword(event.target.value)} />
+        <button onClick={handleShowPassword} type="button"><i className="fa fa-eye" aria-hidden="true"></i></button>
         <button>Acceder</button>
         {message && <p style={{ color: "green" }}>{message}</p>}
         {error && <p style={{ color: "red" }}>{error}</p>}
